47bae2d57f51fe2d164cac30d300e3afdba43c7e
Henrique Pedra <henrique.pedra@hotmail.com>
2022-07-25 19:50:46 -0300

    Resolução exercicio 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes/exercicio-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes/exercicio-1.js
@@ -1,11 +1,12 @@
 function palindromo(string) {
   for (let index in string) {
-    if (string[index] == string[string.length - 1 - index]) {
-      return true;
+    if (string[index] !== string[(string.length - 1)- index]) {
+      return false;
     }
   }
 
-  return false;
+  return true;
 }
 
 // console.log(palindromo('arara'));
+// console.log(palindromo('desenvolvimento'));
