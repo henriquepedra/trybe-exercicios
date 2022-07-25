fe9bc5f20784a9e645f307df68b99282fd6ff213
Henrique Pedra <henrique.pedra@hotmail.com>
2022-07-25 19:18:13 -0300

    Substitui variável declarada nos exércicios 3 e 4. Resolução exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for-in/exercicio-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for-in/exercicio-3.js
@@ -2,10 +2,9 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
 
-info['recorrente'] = 'Sim';
-
-for (let index in info) {
-  console.log(index);
+for (let key in info) {
+  console.log(key);
 }
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for-in/exercicio-4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-e-for-in/exercicio-4.js
@@ -2,10 +2,9 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
 
-info['recorrente'] = 'Sim';
-
-for (let index in info) {
-  console.log(info[index]);
+for (let key in info) {
+  console.log(info[key]);
 }
