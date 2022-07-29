2a3029b08a62106beb7367d42014498287af7250
Henrique Pedra <henrique.pedra@hotmail.com>
2022-07-29 19:27:41 -0300

    Resolução Exercício 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -15,6 +15,7 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
+// Exercício 1
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function calendario() {
@@ -42,3 +43,38 @@ function calendario() {
 }
 
 calendario();
+
+// Exercício 2
+function holidayButton(string) {
+  let buttonClass = document.querySelector('.buttons-container');
+  let button = document.createElement('button');
+  let buttonID = 'btn-holiday';
+
+  button.innerHTML = string;
+  button.id = buttonID;
+
+  buttonClass.appendChild(button);
+}
+
+holidayButton('Feriados');
+
+// Exercício 3
+function showHolidays() {
+  let holidayButton = document.querySelector('#btn-holiday');
+  let holidays = document.querySelectorAll('.holiday');
+  let bgColor = 'rgb(238,238,238)';
+  let color = 'white';
+
+
+  holidayButton.addEventListener('click', function () {
+    for (let index = 0; index < holidays.length; index += 1) {
+      if (holidays[index].style.backgroundColor === color) {
+        holidayButton[index].style.backgroundColor = bgColor;
+      } else {
+        holidays[index].style.backgroundColor = color;
+      }
+    }
+  });
+}
+
+showHolidays();
